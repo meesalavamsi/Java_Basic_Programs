afaeb32c8ed02424c2b0496118a85dd9eb220cd4
Vamsi Meesala <156990795+meesalavamsi@users.noreply.github.com>
2025-09-20 10:16:06 +0530

    Update FirstP.java

--- FirstP.java
+++ FirstP.java
@@ -1,5 +1,18 @@
+/*Java Syntax
+we created a Java file called Main.java, and we used the following code to print "My Name is Vamsi Meesala,this is my first code." to the screen:
+
+The main Method
+The main() method is required in every Java program. It is where the program starts running:
+
+public static void main(String[] args)
+Any code placed inside the main() method will be executed.
+
+For now, you don't need to understand the keywords public, static, and void. You will learn about them later in this tutorial. Just remember: main() is the starting point of every Java program.
+
+ */ 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    System.out.println("My Name is Vamsi Meesala,this is my first code.");
   }
+
 }
